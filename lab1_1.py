b2798590ab64c40635966cd91a6e06c44b9cd9ad
Альбинас <gapshis@bk.ru>
2025-04-04 20:13:51 +0700

    лабораторная работа 1 раздел 1 задание 2

--- lab1_1.py
+++ lab1_1.py
@@ -1,4 +1,19 @@
 #Задание 1.1
+# def get_number(prompt):
+#     while True:
+#         try:
+#             return float(input(prompt))
+#         except ValueError:
+#             print("Ошибка: введите число!")
+
+# a = get_number("Введите первое число: ")
+# b = get_number("Введите второе число: ")
+# c = get_number("Введите третье число: ")
+
+# min_num = min(a, b, c)
+# print("Минимальное число:", min_num)
+
+#Задание 1.2
 def get_number(prompt):
     while True:
         try:
@@ -6,9 +21,9 @@ def get_number(prompt):
         except ValueError:
             print("Ошибка: введите число!")
 
-a = get_number("Введите первое число: ")
-b = get_number("Введите второе число: ")
-c = get_number("Введите третье число: ")
+nums = [get_number(f"Введите число {i+1}: ") for i in range(3)]
 
-min_num = min(a, b, c)
-print("Минимальное число:", min_num)
+print("Числа в интервале [1, 50]:")
+for num in nums:
+    if 1 <= num <= 50:
+        print(num)
